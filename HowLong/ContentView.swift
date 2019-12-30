4cf53be7a7ca2290567ffdee977ab5077a7fdae0
YuHao <sgcob187575@gmail.com>
2019-12-30 20:47:56 +0800

    Initial Commit

---
+++ HowLong/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowLong
+//
+//  Created by 陳昱豪 on 2019/12/30.
+//  Copyright © 2019 Chen_Yu_Hao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
